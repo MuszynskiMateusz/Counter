7c9f3ed8192c870b582157cc8eb132f56e56d975
Mateusz Muszyński <mateuszmuszynski@Mateuszs-MacBook-Air.local>
2024-10-08 20:26:40 +0200

    Add CounterApp sln

---
+++ CounterApp/CounterApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Counter;
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClickedAdd(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterValue.Text = $"Clicked {count} time";
+        else
+            CounterValue.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterValue.Text);
+    }
+
+    private void OnCounterClickedDelete(object sender, EventArgs e)
+    {
+        count--;
+
+        if (count == 1)
+            CounterValue.Text = $"Clicked {count} time";
+        else
+            CounterValue.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterValue.Text);
+    }
+}
